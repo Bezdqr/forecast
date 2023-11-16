886e0df2386162aa8d534365390343ef9003b647
Bezdqr <dima05022008@gmail.com>
2023-11-16 20:35:45 +0300

    forecast

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import requests
 
-url = "https://wttr.in"
-params = {
-    "Tn": "",
-    "lang": "ru"
-}
 
-response = requests.get(url, params=params)
-print(response.text)
+def main():
+    url = "https://wttr.in"
+    params = {
+        "Tn": "",
+        "lang": "ru"
+    }
+
+    response = requests.get(url, params=params)
+    print(response.text)
+
+
+if __name__ == "__main__":
+    main()
